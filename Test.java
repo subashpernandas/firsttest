42cd92ea6a6fb3bf74f52fafd63ba54a0f3f39d4
subashpernandas <84831317+subashpernandas@users.noreply.github.com>
2022-10-31 16:39:39 +0530

    add method

--- Test.java
+++ Test.java
@@ -4,5 +4,11 @@ public static void main(String args[]){
 
 Sysem.out.println("Hello Test From Dev Branch::::::::::::");
 System.out.println("Latest Changes Done Here....");
+  Test t = new Test();
+  int returnSum = t.add(4,6);
+  System.out.println(returnSum);
 }
+  
+  public static int add(int a, int b){
+    return a+b;
 }
